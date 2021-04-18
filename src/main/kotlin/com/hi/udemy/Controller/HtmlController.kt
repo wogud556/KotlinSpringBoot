1ac0025b6d62aa7d81a86cdbd410e10c73752232
wogud556 <wogud556@naver.com>
2021-04-18 23:52:59 +0900

    20210418 kotlin springboot

--- src/main/kotlin/com/hi/udemy/Controller/HtmlController.kt
+++ src/main/kotlin/com/hi/udemy/Controller/HtmlController.kt
@@ -2,16 +2,23 @@ package com.hi.udemy.Controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class HtmlController {
 
-    @RequestMapping("/")
+    @GetMapping("/") //@GetMapping 일때
     fun index(model:Model) : String {
 //        return ''
 //        println("index method")
         return "index" // 메소드가 index라는 것을 인지하면 template 하위에 있는 index.html을 읽어 들어온다.(jsp 불러오는방식과 동일함)
+    }
+
+    @GetMapping("/post/{num}") //@GetMapping 일때
+    fun post(model: Model, @PathVariable num : Int){ // PathVariable url에서 받아오는 데이터를 가져옴
+        println("num:\t${num}")
     }
 }
 
